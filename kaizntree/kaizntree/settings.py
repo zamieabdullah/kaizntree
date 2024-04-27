253c2cf3ecc688b2778d2e0e99bbce8fc79accdd
zamieabdullah <zamieabdullah0@gmail.com>
2024-04-26 23:21:43 -0400

    env changes 4

--- kaizntree/kaizntree/settings.py
+++ kaizntree/kaizntree/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
