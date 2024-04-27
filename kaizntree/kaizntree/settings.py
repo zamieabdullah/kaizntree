3219fa9596aa251addd4c2986c5dd8b4a0952a54
zamieabdullah <zamieabdullah0@gmail.com>
2024-04-26 22:54:47 -0400

    env changes 2

--- kaizntree/kaizntree/settings.py
+++ kaizntree/kaizntree/settings.py
@@ -110,10 +110,10 @@ WSGI_APPLICATION = 'kaizntree.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env('DATABASE_NAME'),
-        'USER': env('DATABASE_USER'),
-        'PASSWORD': env('DATABASE_PASSWORD'),
-        'HOST': env('DATABASE_HOST'),
+        'NAME': os.environ.get('DATABASE_NAME'),
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': os.environ.get('DATABASE_HOST'),
         'PORT': '5432'
     }
 }
